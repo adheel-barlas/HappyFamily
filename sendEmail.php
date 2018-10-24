6a4f6f93a0df884c625ce4cf664476a370364f0a
Adheel Barlas <adheelb8@gmail.com>
2018-10-24 21:51:48 +0100

    testing smtp with generic address

--- sendEmail.php
+++ sendEmail.php
@@ -1,11 +1,31 @@
 <?php
+require('PHPMailer/class.phpmailer.php');
 
-    $to = 'adheelb@hotmail.com';
-    $subject = $_REQUEST['subject'];
+$mail = new PHPMailer();
 
-    // NOT SUGGESTED TO CHANGE THESE VALUES
-    $message = $_REQUEST['message'];
-    $headers = 'From: ' . $_REQUEST['email'] . PHP_EOL ;
-    mail ( $to, $subject, $message, $headers ) ;
+$mail->IsSMTP();                                      // set mailer to use SMTP
+$mail->Host = "smtp.1and1.com";  // specify main and backup server
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->Username = "u95235794";  // SMTP username
+$mail->Password = "H@ppyF@m1ly"; // SMTP password
 
+$mail->From = "info@kcompass.net";
+$mail->FromName = "Info - Kcompass";
+$mail->AddAddress("adheelb@hotmail.com");                  // name is optional
+
+$mail->IsHTML(true);                                  // set email format to HTML
+
+$mail->Subject = "Here is the subject";
+$mail->Body    = "This is the HTML message body in bold!";
+$mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+
+if(!$mail->Send())
+{
+   echo "Message could not be sent. 
+";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+
+echo "Message has been sent";
 ?>
